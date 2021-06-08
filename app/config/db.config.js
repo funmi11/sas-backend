b8e641c516573d5f997d639ac6adf2500bda3489
useful <useful.kyo@gmail.com>
2021-06-08 15:51:13 -0700

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,27 +1,27 @@
-module.exports = {
-    HOST : 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'sas',
-    dialect: 'mysql',
-    pool: {
-        max: 1000,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-};
-
 // module.exports = {
-//   HOST: "localhost",
-//   USER: "onigege",
-//   PASSWORD: "gege@CODEtech2019",
-//   DB: "diamondFMReport",
-//   dialect: "mysql",
-//   pool: {
-//     max: 1000,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
+//     HOST : 'localhost',
+//     USER: 'root',
+//     PASSWORD: '',
+//     DB: 'sas',
+//     dialect: 'mysql',
+//     pool: {
+//         max: 1000,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
 // };
+
+module.exports = {
+  HOST: "localhost",
+  USER: "useful",
+  PASSWORD: "kareem1987yusufOLATAYO@CODEtech2019",
+  DB: "sas",
+  dialect: "mysql",
+  pool: {
+    max: 1000,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
